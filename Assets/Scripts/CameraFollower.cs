69dcfe8a1a96908a0246d9645c751cc01e641de6
alexa <alexadecimal@icloud.com>
2021-08-29 21:45:42 -0500

    Camera follows player

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    public GameObject character;
-
+    private GameObject character;
     public float smoothSpeed = 5f;
-
-    private Vector3 mDistance;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        mDistance = character.transform.position - transform.position;
-    }
+    private Vector3 mDistance = new Vector3(-5,-4,-3);
+    private bool assigned = false;
 
     void LateUpdate()
     {
+        if (assigned==false)
+        {
+            assigned=true;
+            character = GameObject.FindGameObjectWithTag("Player");
+        }
         Vector3 nextPos = character.transform.position - mDistance;
         transform.position = Vector3.Lerp(transform.position, nextPos, smoothSpeed * Time.deltaTime);
     }
